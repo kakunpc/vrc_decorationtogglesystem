8943410fbbd08592b1d6dbbfc903cf6b24612883
kakunpc <15257475+kakunpc@users.noreply.github.com>
2024-01-05 20:12:08 +0900

    ビルドできるように設定

--- release_build.bat
+++ release_build.bat
@@ -1,4 +1,2 @@
-
-REM AssetsからPackageへ移動
 move Assets\kakunvr\DecoToggleSystem Packages\com.kakunvr.decotogglesystem
 Powershell Compress-Archive -Path Packages\com.kakunvr.decotogglesystem\* -DestinationPath com.kakunvr.decotogglesystem.zip
